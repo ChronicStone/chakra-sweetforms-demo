a87f87e8b4bd1f5f6f5f097116708f8f1ca3b9e4
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2021-03-14 21:46:45 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import App from "./App.vue";
+import Chakra, {
+  CThemeProvider,
+  CColorModeProvider,
+  CBox,
+  CReset
+} from "@chakra-ui/vue";
+
+// Import Chakra UI Plugin and register it.
+Vue.use(Chakra);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render(h) {
+    // Mount our application inside the
+    // ThemeProvider and ColorModeProvider components :)
+
+    // You can also do this inside the App.vue file if you wish :)
+    // See example at https://github.com/chakra-ui/chakra-ui-vue#usage
+    return h(CThemeProvider, [
+      h(CColorModeProvider, [h(CBox, [h(CReset), h(App)])])
+    ]);
+  }
+}).$mount("#app");
